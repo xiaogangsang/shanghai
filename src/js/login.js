b24a5aa496c693e9b7fa59c9733c3ec806d8e716
Kyle Lu <orion6sky@gmail.com>
2016-05-16 14:24:47 +0800

    没登陆不能看index页

--- src/js/login.js
+++ src/js/login.js
@@ -4,12 +4,13 @@ var common = require('common');
 
 $(function () {
 
-  if (Cookies.get('Xtoken') != undefined) {
-    window.location.href = 'index.html';
-  }
-
   if (window.location.search.substring(1).indexOf('logout') > -1) {
     common.logout();
+    return false;
+  }
+
+  if (Cookies.get('Xtoken') != undefined) {
+    window.location.href = 'index.html';
   }
 
   $('#form-login').parsley();
