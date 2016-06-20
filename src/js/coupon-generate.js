96eec7e4ad0b231edc0d7d4effa50649363d325d
Kyle Lu <orion6sky@gmail.com>
2016-06-20 15:08:12 +0800

    产码页

--- src/js/coupon-generate.js
+++ src/js/coupon-generate.js
@@ -1,7 +1,6 @@
 'use strict;'
 
 var common = require('common');
-var _budgetSource = [];
 var _pageIndex = 1;
 var _pageSize = 10;
 var _pageTotal = 0;
@@ -10,65 +9,36 @@ var searchCache = {};
 
 $(function () {
   common.init('coupon-generate');
+  $('#formGenerate').parsley();
+  loadHistory();
 });
 
-$('#formSearch').on('click', 'button[type=submit]', function (event) {
+$('#formGenerate').on('submit', function (event) {
   event.preventDefault();
-});
-
-$('#formSearch').on('submit', function (e) {
-  e.preventDefault();
-  var sendData = {
-    couponId: $.trim($('#search_couponId').val()),
-    name: $.trim($('#search_name').val()),
-    budgetSource: $('#search_budgetSource').val(),
-    pageSize: _pageSize,
-  };
-  if (!!_querying) {
+  if (!!_querying || $(this).parsley().isValid() == false) {
     return false;
   }
 
-  _querying = true;
-  if (useCache) {
-    sendData = searchCache;
-  } else {
-    searchCache = sendData;
-  }
-
-  sendData.pageIndex = _pageIndex;
+  $('#formGenerate button').prop('disabled', true).text('提交中');
 
   $.ajax({
-    url: common.API_HOST + 'coupon/couponList',
+    // url: common.API_HOST + 'couponCode/produceCodes',
+    url: 'http://172.16.0.50:8080/movie-ops/couponCode/produceCodes',
     type: 'POST',
     dataType: 'json',
-    data: sendData,
+    data: {
+      couponId: $.trim($('#couponId').val()),
+      amount: $.trim($('#amount').val()),
+    },
   })
   .done(function (res) {
     _querying = false;
+    $('#formGenerate button').prop('disabled', false).text('产码');
     if (!!~~res.meta.result) {
-      if (res.data == null || res.data.rows.length < 1) {
-        $('#dataTable tbody').html('<tr><td colspan="7" align="center">查不到相关数据，请修改查询条件！</td></tr>');
-        $('#pager').html('');
-        $('#btn-export').prop('disabled', true);
-      } else {
-        useCache = true;
-        _pageIndex = res.data.pageIndex;
-        _pageTotal = Math.ceil(res.data.total / _pageSize);
-        setPager(res.data.total, _pageIndex, res.data.rows.length, _pageTotal);
-
-        _(res.data.rows).forEach(function (item, key) {
-          _(_budgetSource).forEach(function (group, key) {
-            _(group).forEach(function (source, key) {
-              if (item.budgetSource == source.id) {
-                item.budgetSource = source.sourceName;
-              }
-            });
-          });
-        });
-
-        setTableData(res.data.rows);
-        $('#btn-export').prop('disabled', false);
-      }
+      alert('产码请求已提交，可在下面列表中查看进度');
+      $('#couponId, #amount').text('');
+      _pageIndex = 1;
+      loadHistory();
     } else {
       alert('接口错误：' + res.meta.msg);
     }
@@ -77,94 +47,51 @@ $('#formSearch').on('submit', function (e) {
   return false;
 });
 
-$(document).on('change', '#search_level', function (event) {
-  event.preventDefault();
-  var level = $(this).val();
-  if (level == undefined || level == '') {
-    $('#search_budgetSource').html('<option value="">全部</option>');
-    $('#search_budgetSource').closest('.form-group').hide();
-  } else {
-    var sources = [];
-    _(_budgetSource).forEach(function (group, key) {
-      if (level == key) {
-        sources = group;
-      }
-    });
-
-    if (sources.length < 1) {
-      $('#search_budgetSource').html('<option value="">全部</option>');
-      $('#search_budgetSource').closest('.form-group').hide();
-      alert('所选成本中心类别下无成本中心，这个情况不正常，需要注意哦！');
-    } else {
-      var html = '';
-      _(sources).forEach(function (source) {
-        html += '<option value="' + source.id + '">' + source.sourceName + '</option>';
-      });
-
-      $('#search_budgetSource').html(html);
-      $('#search_budgetSource').closest('.form-group').show();
-    }
-  }
-});
-
-$(document).on('click', '.multi-check-all', function () {
-  var items = $(this).closest('table').find('.multi-check');
-  if ($(this).prop('checked')) {
-    items.prop('checked', true);
-  } else {
-    items.prop('checked', false);
-  }
-});
-
-$(document).on('click', '#btn-online-multi, #btn-offline-multi', function (e) {
-  e.preventDefault();
-  if ($('.multi-check:checked').length < 1) {
-    alert('请至少选中一个！');
+function loadHistory() {
+  if (!!_querying) {
     return false;
   }
 
-  var queryStatusName = $(this).attr('id') == 'btn-online-multi' ? '上线' : '下线';
-  if (window.confirm('确定要' + queryStatusName + '选中的单元吗？')) {
-    var queryStatus = $(this).attr('id') == 'btn-online-multi' ? 1 : 0;
-    var ids = [];
-    var $checkedItems = $('.multi-check:checked');
-    $checkedItems.each(function () {
-      ids.push($(this).closest('tr').data('id'));
-    });
+  _querying = true;
 
-    var ajaxUrl = queryStatus == 1 ? 'activity/activityOnline' : 'activity/activityOffline';
-
-    $.ajax({
-      url: common.API_HOST + ajaxUrl,
-      type: 'POST',
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(ids),
-    })
-    .done(function (res) {
-      if (!!~~res.meta.result) {
-        $('#formSearch').trigger('submit');
-        queryStatus == 1 ? alert('批量上线成功') : alert('批量下线成功');
-        $('.multi-check-all').prop('checked', false);
+  $.ajax({
+    // url: common.API_HOST + 'couponCode/produceHistory',
+    url: 'http://172.16.0.50:8080/movie-ops/couponCode/produceHistory',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      pageIndex: _pageIndex,
+      pageSize: _pageSize,
+    },
+  })
+  .done(function (res) {
+    _querying = false;
+    if (!!~~res.meta.result) {
+      if (res.data == null || res.data.rows.length < 1) {
+        $('#dataTable tbody').html('<tr><td colspan="7" align="center">载入中...</td></tr>');
+        $('#pager').html('');
       } else {
-        alert('接口错误：' + res.meta.msg);
+        _pageIndex = res.data.pageIndex;
+        _pageTotal = Math.ceil(res.data.total / _pageSize);
+        setPager(res.data.total, _pageIndex, res.data.rows.length, _pageTotal);
+
+        _(res.data.rows).forEach(function (item, key) {
+          var date = new Date(item.createTime);
+          item.createTime = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+          if (item.status == 'SUCCESS') {
+            item.canExport = true;
+          } else {
+            item.canExport = false;
+          }
+        });
+
+        setTableData(res.data.rows);
       }
-    });
-  }
-
-  return false;
-});
-
-$(document).on('click', '#btn-export', function (event) {
-  event.preventDefault();
-  var exportUrl = common.API_HOST + 'coupon/exportCoupons?' +
-  'couponId=' + $.trim($('#search_couponId').val()) +
-  '&name=' + $.trim($('#search_name').val()) +
-  '&budgetSource=' + $('#search_budgetSource').val() +
-  '&pageSize=' + _pageSize +
-  '&pageIndex=' + _pageIndex;
-  window.location.href =  exportUrl;
-});
+    } else {
+      alert('接口错误：' + res.meta.msg);
+    }
+  });
+}
 
 $('#pager').on('click', '.prev,.next', function (e) {
   e.preventDefault();
@@ -186,7 +113,7 @@ $('#pager').on('click', '.prev,.next', function (e) {
     _pageIndex++;
   }
 
-  $('#formSearch').trigger('submit');
+  loadHistory();
   return false;
 });
 
@@ -203,7 +130,7 @@ $('#pager').on('click', '#btn-pager', function (e) {
   }
 
   _pageIndex = pageNo;
-  $('#formSearch').trigger('submit');
+  loadHistory();
   return false;
 });
 
