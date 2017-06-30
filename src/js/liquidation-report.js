2de090455bdb51e91c08a1bb063cd17f6b15f531
ShawnLin <lsgbhp@gmail.com>
2017-06-30 15:30:51 +0800

    update

--- src/js/liquidation-report.js
+++ src/js/liquidation-report.js
@@ -1,42 +1,79 @@
 'use strict;'
 var common = require('common');
 var settlementCommon = require('settlementCommon');
+var pager = require('pager');
+
+var _useCache = false;
+var _formCache = {};
+var _querying = false;
 
 var reportTable = {
-  table: $('#reportTable'), 
+  table: $('#reportTable'),
   keyMap : [
-    {label: '文件名', key: 'chargeMerchantNo'},
-    {label: '生成时间', key: 'acquiringOrderType', parseKey: '.'},
+    {label: '生成时间', key: 'createTime', parseKey: function(item) {
+    	return common.getDate(new Date(item.createTime));
+    }},
+    {label: '文件名', key: 'reportName'},
     {label: '操作', parseKey: function(item) {
-    	return '<a class="download" data-url="' + item.fileUrl + '">下载</a>'
+    	return '<button class="btn btn-xs btn-default btn-download" data-url="' + item.reportPath + '">下载</button>';
     }}
   ],
 };
 
 $(function() {
 	common.init('liquidation-report');
-	$('#formSearch').parsley();
+	pager.init($('#pager'));
+
+	pager.pageIndex = 1;
+	$('#formSearch').trigger('submit');
+	$('#formReport').parsley();
+
 	settlementCommon.setupDatetimePicker($('#report_startTime'));
 	settlementCommon.setupDatetimePicker($('#report_endTime'));
   settlementCommon.formatTableWithData(reportTable);
+});
+
+$('#formSearch').on('submit', function(e) {
+	e.preventDefault();
+
+	if (_querying) return false;
+	_querying = true;
+
+	var param = {
+		pageSize: pager.pageSize,
+		pageIndex: pager.pageIndex,
+	}
+
+	if (_useCache) {
+		param = _formCache;
+		param.pageIndex = pager.pageIndex;
+	} else {
+		_formCache = param;
+	}
 
   $.ajax({
     url: common.API_HOST + 'settlement/genReport/listReports',
     type: 'GET',
     dateType: 'json',
+    data: param
   }).done(function(res) {
-    handleData(res);
+  	// res = JSON.parse(res);
+    // if (settlementCommon.prehandleData(res)) {
+   	if (!!~~res.meta.result) {
+      pager.pageTotal = Math.ceil(res.data.reportCount / pager.pageSize);
+      pager.setPager(res.data.reportCount, pager.pageIndex, res.data.reportList.length, pager.pageTotal);
+
+      settlementCommon.formatTableWithData(reportTable, res.data.reportList);
+    }
+  }).always(function(res) {
+  	_querying = false;
   });
 });
 
-function handleData(res) {
-	console.log(res)
-}
-
-$('#formSearch').on('submit', function (e) {
+$('#formReport').on('submit', function (e) {
 	e.preventDefault()
 
-  if (!$('#formSearch').parsley().isValid()) {
+  if (!$('#formReport').parsley().isValid()) {
     return false;
   }
 
@@ -56,8 +93,8 @@ $('#formSearch').on('submit', function (e) {
 			break;
 	}
 	genReport(reportUrl, {
-  	startDate: $('report_startTime').val(),
-  	endDate: $('report_endTime').val(),
+  	startDate: $('#report_startTime').val(),
+  	endDate: $('#report_endTime').val(),
   	channelId: 3,
   });
 });
@@ -70,7 +107,20 @@ function genReport(reportUrl, reportParam) {
 		data: reportParam,
 	})
 	.done(function(res) {
-		console.log(res);
+		if (!!~~res.meta.result) {
+			alert('操作成功，请稍后刷新页面查询');
+			$('#formSearch').trigger('submit');
+		} else {
+			alert(res.meta.msg);
+		}
 	})
 }
+
+$('#reportTable').on('click', '.btn-download', function(e) {
+	e.preventDefault();
+
+	var url = $(this).data('url');
+	console.log(url);
+  window.location.href = common.API_HOST + 'settlement/downloadFile/downloadReport?path=' + url;
+});
 
